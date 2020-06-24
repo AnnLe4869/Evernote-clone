db93a80ac44caa70ceb313167d3487cee4db0c1e
Anh Le <kunquan2345@gmail.com>
2020-06-24 13:53:29 -0700

    Add firebase config

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: "evernote-e077e.firebaseapp.com",
+  databaseURL: "https://evernote-e077e.firebaseio.com",
+  projectId: "evernote-e077e",
+  storageBucket: "evernote-e077e.appspot.com",
+  messagingSenderId: "333330020976",
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
