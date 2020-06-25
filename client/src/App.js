9ec6ecdda097430033edeb78f967ccda69cbe910
Anh Le <kunquan2345@gmail.com>
2020-06-24 21:27:27 -0700

    Add CSS style

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import firebase from "firebase";
 import "./App.css";
 
+const db = firebase.firestore();
 function App() {
+  const [selectedNoteIndex, setSelectedNoteIndex] = useState(null);
+  const [selectedNote, setSelectedNote] = useState(null);
+  const [notes, setNotes] = useState(null);
+
+  useEffect(() => {
+    db.collection("notes").onSnapshotsInSync();
+    return () => {};
+  }, []);
+
   return <div className="App">hello</div>;
 }
 
