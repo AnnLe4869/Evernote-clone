e4da4f6b8e20750c9f827cee105919a66b1e592e
Anh Le <kunquan2345@gmail.com>
2020-06-26 20:54:56 -0700

    Add OAuth in the website
    _ One thing unclear is that we still need to import firebaseui for the page to work properly

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,139 +1,34 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-import { makeStyles } from "@material-ui/core/styles";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import {
-  Avatar,
-  Button,
-  CssBaseline,
-  TextField,
-  FormControlLabel,
-  Checkbox,
-  Link,
-  Grid,
-  Box,
-  Typography,
-  Container,
-} from "@material-ui/core";
+import { Button, CssBaseline, Container } from "@material-ui/core";
 
 import firebase from "firebase/app";
-import * as firebaseui from "firebaseui";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(2, 0, 2),
-  },
-}));
+import "firebaseui";
 
 export default function Auth() {
-  const classes = useStyles();
-  const uiConfig = {
-    signInSuccessUrl: "/main",
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    ],
+  const history = useHistory();
+
+  const handleClick = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    try {
+      const result = await firebase.auth().signInWithPopup(provider);
+      //const token = result.credential.accessToken;
+      const user = result.user;
+      console.log(user);
+      history.push("/main");
+    } catch (err) {
+      console.error(err);
+    }
   };
-  useEffect(() => {
-    // const provider = new firebase.auth.GoogleAuthProvider();
-    // firebase
-    //   .auth()
-    //   .signInWithPopup(provider)
-    //   .then(function (result) {
-    //     const token = result.credential.accessToken;
-    //     const user = result.user;
-    //   })
-    //   .catch(function (error) {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //     const email = error.email;
-    //     const credential = error.credential;
-    //   });
-    // const ui = new firebaseui.auth.AuthUI(firebase.auth());
-    // ui.start("#authentication", uiConfig);
-  }, [uiConfig]);
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h4">
-          Sign in
-        </Typography>
-        <form className={classes.form}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
+      <div>
+        <Button variant="contained" color="secondary" onClick={handleClick}>
+          Sign in with Google
+        </Button>
       </div>
     </Container>
   );
