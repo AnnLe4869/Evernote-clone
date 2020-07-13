6805444d22f11891a85b5a617c6c8845e575aed3
Anh Le <kunquan2345@gmail.com>
2020-07-13 04:43:08 -0700

    FIX: styling the scroll bar of the NoteList

--- client/src/pages/Content/NoteList/index.js
+++ client/src/pages/Content/NoteList/index.js
@@ -52,6 +52,19 @@ const useStyles = makeStyles((theme) => ({
     height: "calc(100vh - 8rem)",
     overflowY: "auto",
     boxSizing: "content-box",
+    scrollbarColor: "transparent",
+    scrollbarWidth: "thin",
+    "&::-webkit-scrollbar": {
+      width: "0.4em",
+    },
+    "&::-webkit-scrollbar-track": {
+      boxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+      webkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(0,0,0,.1)",
+      outline: "1px solid slate grey",
+    },
   },
 }));
 
