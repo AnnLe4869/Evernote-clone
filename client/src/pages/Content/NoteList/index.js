f447b09310e3f6478f98b2e744fb1e786fec86a5
Anh Le <kunquan2345@gmail.com>
2020-07-18 04:16:47 -0700

    Add the dialog which show list of notebook to move to

--- client/src/pages/Content/NoteList/index.js
+++ client/src/pages/Content/NoteList/index.js
@@ -1,13 +1,27 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
 
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
@@ -43,6 +57,27 @@ const useStyles = makeStyles((theme) => ({
     bottom: theme.spacing(0),
     marginRight: theme.spacing(1),
   },
+  dialog: {
+    overflow: "hidden",
+  },
+  dialogItemDisplay: {
+    height: "calc(100vh - 64px - 230px)",
+    overflowY: "auto",
+    boxSizing: "content-box",
+    scrollbarColor: "transparent",
+    scrollbarWidth: "thin",
+    "&::-webkit-scrollbar": {
+      width: "0.4em",
+    },
+    "&::-webkit-scrollbar-track": {
+      boxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+      webkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(0,0,0,.1)",
+      outline: "1px solid slate grey",
+    },
+  },
   itemPrimaryText: {
     fontSize: 14,
     fontWeight: 600,
@@ -77,10 +112,28 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(1),
   },
 }));
+// For the Share button
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
+});
 
 export default function NoteList() {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [dialogOpenStatus, setDialogOpenStatus] = useState(false);
+  //const [isAllowEdit, setIsAllowEdit] = useState(false);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClickOpenDialog = () => {
+    setDialogOpenStatus(true);
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpenStatus(false);
+    setAnchorEl(null);
+  };
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -89,6 +142,7 @@ export default function NoteList() {
   const handleClose = () => {
     setAnchorEl(null);
   };
+
   return (
     <div>
       <List className={classes.root}>
@@ -116,9 +170,11 @@ export default function NoteList() {
           >
             Notebook title
           </Typography>
+          {/* This is the utility button */}
           <IconButton className={classes.headerAction} onClick={handleClick}>
             <MoreHorizIcon />
           </IconButton>
+          {/* This is the dropdown menu when we click the utility button on list header */}
           <Menu
             id="simple-menu"
             anchorEl={anchorEl}
@@ -128,11 +184,80 @@ export default function NoteList() {
             anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
             transformOrigin={{ vertical: "top", horizontal: "right" }}
           >
-            <MenuItem onClick={handleClose}>Share notebook</MenuItem>
+            <MenuItem onClick={handleClickOpenDialog}>Move notebook</MenuItem>
             <MenuItem onClick={handleClose}>Share notebook</MenuItem>
             <MenuItem onClick={handleClose}>Share notebook</MenuItem>
           </Menu>
         </ListItem>
+        {/* This is the dialog that will appear, which show list of all notebook we can move to */}
+        <Dialog
+          open={dialogOpenStatus}
+          onClose={handleCloseDialog}
+          aria-labelledby="form-dialog-title"
+          maxWidth="md"
+          className={classes.dialog}
+        >
+          <DialogTitle id="form-dialog-title">Move note to</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Find the location you want to move the file to or just click the
+              one you want
+            </DialogContentText>
+            <TextField
+              id="email"
+              //autoFocus
+              variant="outlined"
+              size="small"
+              label="Find the location"
+              ///defaultValue="hello world"
+              //className={classes.dialogTextSearch}
+              fullWidth
+            />
+            <List
+              component="nav"
+              aria-label="main"
+              className={classes.dialogItemDisplay}
+            >
+              <ListItem button>
+                <ListItemIcon>
+                  <SvgIcon>
+                    <path
+                      fill="currentColor"
+                      d="M17,4V10L15,8L13,10V4H9V20H19V4H17M3,7V5H5V4C5,2.89 5.9,2 7,2H19C20.05,2 21,2.95 21,4V20C21,21.05 20.05,22 19,22H7C5.95,22 5,21.05 5,20V19H3V17H5V13H3V11H5V7H3M5,5V7H7V5H5M5,19H7V17H5V19M5,13H7V11H5V13Z"
+                    />
+                  </SvgIcon>
+                </ListItemIcon>
+                <ListItemText primary="Inbox" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <SvgIcon>
+                    <path
+                      fill="currentColor"
+                      d="M17,4V10L15,8L13,10V4H9V20H19V4H17M3,7V5H5V4C5,2.89 5.9,2 7,2H19C20.05,2 21,2.95 21,4V20C21,21.05 20.05,22 19,22H7C5.95,22 5,21.05 5,20V19H3V17H5V13H3V11H5V7H3M5,5V7H7V5H5M5,19H7V17H5V19M5,13H7V11H5V13Z"
+                    />
+                  </SvgIcon>
+                </ListItemIcon>
+                <ListItemText primary="Drafts" />
+              </ListItem>
+              {/* The below are just a test */}
+            </List>
+          </DialogContent>
+          <DialogActions>
+            <ThemeProvider theme={theme}>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={handleCloseDialog}
+              >
+                Move
+              </Button>
+              <Button variant="outlined" onClick={handleCloseDialog}>
+                Cancel
+              </Button>
+            </ThemeProvider>
+          </DialogActions>
+        </Dialog>
         <Divider component="li" />
 
         {/* The below are item */}
@@ -248,185 +373,6 @@ export default function NoteList() {
             </Typography>
           </ListItem>
           <Divider component="li" />
-          <ListItem alignItems="flex-start" button className={classes.listItem}>
-            <ListItemText
-              primary={
-                <React.Fragment>
-                  <Typography
-                    component="div"
-                    variant="h6"
-                    color="textPrimary"
-                    className={classes.itemPrimaryText}
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                </React.Fragment>
-              }
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="subtitle2"
-                    color="textSecondary"
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                </React.Fragment>
-              }
-            />
-            <Typography
-              component="span"
-              variant="subtitle2"
-              color="textSecondary"
-              className={classes.itemSubtitle}
-            >
-              Jul 13
-            </Typography>
-          </ListItem>
-          <Divider component="li" />
-          <ListItem alignItems="flex-start" button className={classes.listItem}>
-            <ListItemText
-              primary={
-                <React.Fragment>
-                  <Typography
-                    component="div"
-                    variant="h6"
-                    color="textPrimary"
-                    className={classes.itemPrimaryText}
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                </React.Fragment>
-              }
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="subtitle2"
-                    color="textSecondary"
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                </React.Fragment>
-              }
-            />
-            <Typography
-              component="span"
-              variant="subtitle2"
-              color="textSecondary"
-              className={classes.itemSubtitle}
-            >
-              Jul 13
-            </Typography>
-          </ListItem>
-          <Divider component="li" />
-          <ListItem alignItems="flex-start" button className={classes.listItem}>
-            <ListItemText
-              primary={
-                <React.Fragment>
-                  <Typography
-                    component="div"
-                    variant="h6"
-                    color="textPrimary"
-                    className={classes.itemPrimaryText}
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                </React.Fragment>
-              }
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="subtitle2"
-                    color="textSecondary"
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                </React.Fragment>
-              }
-            />
-            <Typography
-              component="span"
-              variant="subtitle2"
-              color="textSecondary"
-              className={classes.itemSubtitle}
-            >
-              Jul 13
-            </Typography>
-          </ListItem>
-          <Divider component="li" />
-          <ListItem alignItems="flex-start" button className={classes.listItem}>
-            <ListItemText
-              primary={
-                <React.Fragment>
-                  <Typography
-                    component="div"
-                    variant="h6"
-                    color="textPrimary"
-                    className={classes.itemPrimaryText}
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                </React.Fragment>
-              }
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="subtitle2"
-                    color="textSecondary"
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                </React.Fragment>
-              }
-            />
-            <Typography
-              component="span"
-              variant="subtitle2"
-              color="textSecondary"
-              className={classes.itemSubtitle}
-            >
-              Jul 13
-            </Typography>
-          </ListItem>
-          <Divider component="li" />
-          <ListItem alignItems="flex-start" button className={classes.listItem}>
-            <ListItemText
-              primary={
-                <React.Fragment>
-                  <Typography
-                    component="div"
-                    variant="h6"
-                    color="textPrimary"
-                    className={classes.itemPrimaryText}
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                </React.Fragment>
-              }
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="subtitle2"
-                    color="textSecondary"
-                  >
-                    to Scott, Alex, Jennifer
-                  </Typography>
-                </React.Fragment>
-              }
-            />
-            <Typography
-              component="span"
-              variant="subtitle2"
-              color="textSecondary"
-              className={classes.itemSubtitle}
-            >
-              Jul 13
-            </Typography>
-          </ListItem>
         </div>
       </List>
     </div>
