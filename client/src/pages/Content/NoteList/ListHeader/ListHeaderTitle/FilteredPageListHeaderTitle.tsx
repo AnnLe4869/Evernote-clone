48f33e98658701c30272baef39fcbe9e0a1246d6
Anh Le <kunquan2345@gmail.com>
2020-12-21 02:51:51 -0800

    Add StarIcon to indicate notebook in shortcuts

--- client/src/pages/Content/NoteList/ListHeader/ListHeaderTitle/FilteredPageListHeaderTitle.tsx
+++ client/src/pages/Content/NoteList/ListHeader/ListHeaderTitle/FilteredPageListHeaderTitle.tsx
@@ -3,12 +3,19 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import React from "react";
 import useNotebookFromPath from "../../../../../utils/useNotebookFromPath";
+import StarIcon from "@material-ui/icons/Star";
 
 const useStyles = makeStyles((theme) => ({
   headerText: {
     fontSize: "20px",
     fontWeight: 400,
     letterSpacing: "0.03em",
+  },
+  itemStarIcon: {
+    color: "#f5cc05",
+    fontSize: 15,
+    verticalAlign: "-0.1em",
+    marginLeft: theme.spacing(1),
   },
 }));
 
@@ -28,6 +35,9 @@ export default function FilteredPageListHeaderTitle() {
             className={classes.headerText}
           >
             {currentNotebook?.name}
+            {currentNotebook?.inShortcut ? (
+              <StarIcon className={classes.itemStarIcon} />
+            ) : null}
           </Typography>
         </React.Fragment>
       }
